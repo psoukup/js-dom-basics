30d00d4bc3cf9d0a4c34c127a3774c81e1e89742
p <p@ps-MacBook-Air.local>
2016-05-02 11:45:33 -0500

    added index file and more js

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 console.log("sanity check");
+
+var firstParagraph = document.getElementById('main');
+console.log(firstParagraph)
+
+var allUnorderedLists = document.getElementsByTagName('ul');
+console.log(allUnorderedLists);
+
+var firstUnorderedList = allUnorderedLists[0];
+console.log(firstUnorderedList);
+
+var allListItems = document.getElementsByTagName('li');
+console.log(allListItems);
+
+for (var i = 0; i < allListItems.length; i++) {
+  console.log(allListItems[i]);
+}
+
+var evenElements = document.getElementsByClassName('even');
+console.log(evenElements)
+
+var oddElements = document.getElementsByClassName('odd');
+console.log(oddElements);
+
+var firstEvenElement = evenElements[0];
+console.log(firstEvenElement);
+
+var firstOddElement = oddElements[0];
+console.log(firstOddElement);
+
+var main = document.querySelector('#main');
+console.log(main);
+
+var firstOddElement = document.querySelector('.odd');
+console.log(firstOddElement);
